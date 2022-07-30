fb452f30f7899e20833ff9a72363e339ebe94e11
Mustafa Kağan Meriç <mustafakaganmeric@gmail.com>
2022-07-31 01:19:16 +0300

    init commit

---
+++ src/test/java/petStore/petStoreTest_Httpentity.java
@@ -0,0 +1,43 @@
+package petStore;
+
+import org.junit.jupiter.api.Order;
+import org.junit.jupiter.api.Test;
+
+public class petStoreTest_Httpentity {
+    @Test
+    @Order(1)
+    public void getInventoryTest(){
+        
+    }
+
+    @Test
+    @Order(2)
+    public void postOrderTest(){
+
+    }
+
+    @Test
+    @Order(3)
+    public void getOrderByIdTest(){
+
+    }
+
+    @Test
+    @Order(4)
+    public void delOrderByIdTest(){
+
+    }
+
+    @Test
+    @Order(4)
+    public void delOrderByIdTest2(){
+
+    }
+
+    @Test
+    @Order(5)
+    public void getOrderByIdTest2(){
+
+    }
+}
+
